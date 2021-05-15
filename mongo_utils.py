4c611b06e79ae09f262f206ae0e4a2d244d654e7
nitin-bs1996 <43139834+nitin-bs1996@users.noreply.github.com>
2021-05-15 21:49:18 +0530

    mongo utils cleanup commit

--- mongo_utils.py
+++ mongo_utils.py
@@ -1,7 +1,15 @@
-from pymongo import MongoClient
-from bson import ObjectId
-import configparser
-config = configparser.ConfigParser()
+try:
+    from pymongo import MongoClient
+    from bson import ObjectId
+    import traceback
+    import configparser
+except ImportError as e:
+    traceback.print_exc()
+    print('Failed to import some of the modules.')
+    raise RuntimeError('Failed to import some of the modules.')
+
+
+config = configparser.ConfigParser() #To read mongo credentials from the mongo.config file
 config.read('mongo.config')
 hostname=config.get("DEFAULT","hostname")
 username=config.get("DEFAULT","username")
